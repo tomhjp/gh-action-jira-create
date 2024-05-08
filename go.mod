db037589b6c75c5bc423fa0c2f74ca903a578750
Tom Proctor <tomhjp@users.noreply.github.com>
2024-05-08 17:53:36 +0100

    Replace tomhjp module dep

--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
-module github.com/tomhjp/gh-action-jira-create
+module github.com/hashicorp/gh-action-jira-create
 
 go 1.14
 
 require (
+	github.com/hashicorp/gh-action-jira v0.0.0-20240508131258-e2edc116d3d2
 	github.com/stretchr/testify v1.6.1
-	github.com/tomhjp/gh-action-jira v0.2.0
 )
--- main.go
+++ main.go
@@ -9,10 +9,10 @@ import (
 	"net/url"
 	"os"
 
-	"github.com/tomhjp/gh-action-jira/config"
-	"github.com/tomhjp/gh-action-jira/format"
-	"github.com/tomhjp/gh-action-jira/gha"
-	"github.com/tomhjp/gh-action-jira/jira"
+	"github.com/hashicorp/gh-action-jira/config"
+	"github.com/hashicorp/gh-action-jira/format"
+	"github.com/hashicorp/gh-action-jira/gha"
+	"github.com/hashicorp/gh-action-jira/jira"
 )
 
 func main() {
